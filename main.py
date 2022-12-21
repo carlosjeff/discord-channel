b7aeae7dee12569f52e333b0388a06b9bfe27271
Carlos Jefferson Braga Alves <carlos_jefferson_braga@outlook.com>
2022-12-21 11:11:21 -0400

    refactor: update main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from logic import discord_client, kafka_message_send
+from dotenv import load_dotenv
 from os import getenv
 
+load_dotenv()
 TOKEN_DISCORD = getenv('TOKEN_DISCORD')
+print(TOKEN_DISCORD)
 kafka_server = kafka_message_send.KafkaMessageSend('localhost', 9091, 'ponto')
 client = discord_client.DiscordClient(kafka_server)
 client.run(TOKEN_DISCORD)
